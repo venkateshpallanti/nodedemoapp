a700a71c159e8c1874dbf1b3b83f17b7ced1f2fd
Prabandh Vasireddy <vprabandh@bitapps.net>
2018-09-01 07:55:03 +0530

    changes saved

---
+++ app.js
@@ -0,0 +1,60 @@
+console.log("test");
+
+
+
+// data types : int float char string boolean
+
+// int 1 2 3 4 
+// float 1.2 2.33333
+// char 'e' 'errete' 'djhdtdutt' '2323'
+// string "sdfsadds" "1222"
+
+// boolean true false
+
+// array morethan one value
+
+// object group of arrays
+
+
+
+abc();
+
+
+
+
+
+//for(init;condition;increment/decr)
+
+
+    
+
+
+function abc(){
+  console.log("function calling");
+  var cc= 10;
+  var xx =20;
+  if(cc>xx){
+       console.log("im in if statement");
+       demo1();
+  }else{
+    console.log("im in else statement");
+      demo2();
+  }
+}
+
+function demo1(){
+    console.log("sdasfdasfdasffasdasasf");
+}
+function demo2(){
+    console.log("asdfasdasdasdffasfdassadfadsfsafsadsadfsad");
+}
+//export abc;
+
+
+
+
+
+
+
+
+
